493e15aafe17091b582e5964ff894e8e2966ad26
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2025-01-22 18:00:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="light_speed",
-    version="0.1.2",
+    version="0.1.3",
     description="A comprehensive physics package for light speed and real-world calculations",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
